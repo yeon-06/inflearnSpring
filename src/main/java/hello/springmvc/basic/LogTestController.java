3db983c0c716bdbeaae647e22e036cf0a15f9bf1
sy <white_la@naver.com>
2021-05-28 13:43:19 +0900

    @Slf4j 어노테이션 활용

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,14 +1,13 @@
 package hello.springmvc.basic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController //@Controller는 return viewResolver 해야하는데 RestController는 HTTP 메시지 바디에 바로 입력
 public class LogTestController {
-	private final Logger log = LoggerFactory.getLogger(getClass());
-	
 	@RequestMapping("/log-test")
 	public String logTest() {
 		String name = "Hello Log Test!";
