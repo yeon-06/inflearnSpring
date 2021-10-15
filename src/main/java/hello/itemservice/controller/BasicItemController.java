f696050abc644b74108794c6e4212a29698c9905
yeon-06 <rnjstldus2@hs.ac.kr>
2021-10-15 16:02:37 +0900

    feat: 상품 등록, 상세 조회 구현
    상품 등록 기능 구현
    상품 상세 조회 기능 구현

--- src/main/java/hello/itemservice/controller/BasicItemController.java
+++ src/main/java/hello/itemservice/controller/BasicItemController.java
@@ -7,9 +7,13 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemParamDto;
 import hello.itemservice.domain.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +28,32 @@ public class BasicItemController {
 		List<Item> items = itemRepository.findAll();
 		model.addAttribute("items", items);
 		return "basic/items";
+	}
+	
+	@GetMapping("/{itemId}")
+	public String item(Model model, @PathVariable long itemId) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "basic/item";
+	}
+	
+	
+	@GetMapping("/add")
+	public String addForm() {
+		return "basic/addForm";
+	}
+	
+	@PostMapping("/add/v1")
+	public String saveV1(@ModelAttribute Item item, Model model) {
+		itemRepository.save(item);
+		model.addAttribute(item);
+		return "basic/item";
+	}
+	
+	@PostMapping("/add")
+	public String saveV2(@ModelAttribute("item") Item item) {
+		itemRepository.save(item);
+		return "basic/item";
 	}
 	
 	// test용 데이터
