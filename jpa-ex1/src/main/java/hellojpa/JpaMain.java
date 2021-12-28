9998bf0f24932b94e0b94bd1c04c6b6f2a89bf48
yeon-06 <rnjstldus2@hs.ac.kr>
2021-12-28 22:49:26 +0900

    feat: Member 테이블에 데이터 insert하는 로직 추가

--- jpa-ex1/src/main/java/hellojpa/JpaMain.java
+++ jpa-ex1/src/main/java/hellojpa/JpaMain.java
@@ -2,12 +2,25 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager entityManager = emf.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("helloJPA");
+
+        // member insert
+        entityManager.persist(member);
+        transaction.commit();
+
         entityManager.close();
         emf.close();
     }
