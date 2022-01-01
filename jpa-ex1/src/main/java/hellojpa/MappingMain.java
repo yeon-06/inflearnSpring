b23d721125cd3654e133a6f64316ffd97a629b55
yeon-06 <rnjstldus2@hs.ac.kr>
2022-01-01 18:27:07 +0900

    feat: GenerationValue 이용해보기

---
+++ jpa-ex1/src/main/java/hellojpa/MappingMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class MappingMain {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
+    private static final EntityTransaction transaction = entityManager.getTransaction();
+
+    public static void main(String[] args) {
+        transaction.begin();
+
+        try {
+            insert();
+            transaction.commit();
+
+        } catch (Exception e) {
+            transaction.rollback();
+
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+
+    private static void insert() {
+        Users user = new Users();
+        user.setName("테스트");
+        entityManager.persist(user);
+    }
+}
---
+++ jpa-ex1/src/main/java/hellojpa/Users.java
@@ -0,0 +1,29 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
