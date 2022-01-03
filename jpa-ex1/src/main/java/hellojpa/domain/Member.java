faf268893e72f822e7d311637a433b6bbf1bb763
yeon-06 <rnjstldus2@hs.ac.kr>
2022-01-03 22:46:40 +0900

    refactor: 영속성 전이, 고아 객체 설정 변경

--- jpa-ex1/src/main/java/hellojpa/domain/Member.java
+++ jpa-ex1/src/main/java/hellojpa/domain/Member.java
@@ -21,7 +21,7 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Locker locker;
 
--- jpa-ex1/src/main/java/hellojpa/domain/Order.java
+++ jpa-ex1/src/main/java/hellojpa/domain/Order.java
@@ -15,11 +15,11 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderTime;
