504b33b863ba4434d71ef5d6ec5a566cb0944870
yeon-06 <rnjstldus2@hs.ac.kr>
2022-01-03 22:32:15 +0900

    feat: cascade 적용 예제

---
+++ jpa-ex1/src/main/java/hellojpa/cascade/CascadeMain.java
@@ -0,0 +1,37 @@
+package hellojpa.cascade;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class CascadeMain {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+    private static final EntityManager entityManager = entityManagerFactory.createEntityManager();
+    private static final EntityTransaction transaction = entityManager.getTransaction();
+
+    public static void main(String[] args) {
+        transaction.begin();
+
+        try {
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            entityManager.persist(parent);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            transaction.rollback();
+
+        } finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
