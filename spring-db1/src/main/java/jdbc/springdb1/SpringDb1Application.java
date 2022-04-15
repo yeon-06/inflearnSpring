c33415e3d93449fd89c7345153503ad3e77a90b3
yeon-06 <rnjstldus2@gmail.com>
2022-04-15 14:25:45 +0900

    feat: spring db project initial

---
+++ spring-db1/src/main/java/jdbc/springdb1/SpringDb1Application.java
@@ -0,0 +1,13 @@
+package jdbc.springdb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDb1Application.class, args);
+	}
+
+}
---
+++ spring-db1/src/test/java/jdbc/springdb1/SpringDb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jdbc.springdb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
