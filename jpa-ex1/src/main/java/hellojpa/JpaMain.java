cb57bb0cff53d0e4071c45b63ae265ae1adc0cc5
yeon-06 <rnjstldus2@gmail.com>
2022-06-14 00:50:27 +0900

    feat: JPA 기본 CRUD

---
+++ jpa-ex1/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,52 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello"); // 하나만 생성해 애플리케이션 전체에서 공유
+        EntityManager entityManager = entityManagerFactory.createEntityManager(); // 고객의 요청이 올때마다 생성 (스레드 간 공유 X)
+        EntityTransaction transaction = entityManager.getTransaction(); // 데이터의 모든 변경은 트랜잭션 내부에서 실행할 것
+
+        transaction.begin();
+
+        try {
+            insert(entityManager);
+            update(entityManager);
+            delete(entityManager);
+
+            transaction.commit();
+
+        } catch (Exception e) {
+            transaction.rollback();
+
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+
+    private static void insert(EntityManager entityManager) {
+        Member member = new Member(1L, "yeonLog");
+        entityManager.persist(member);
+    }
+
+    private static Member select(EntityManager entityManager) {
+        return entityManager.find(Member.class, 1L);
+    }
+
+    private static void delete(EntityManager entityManager) {
+        Member member = select(entityManager);
+        entityManager.remove(member);
+    }
+
+    private static void update(EntityManager entityManager) {
+        Member member = select(entityManager);
+        member.setName("연로그");
+    }
+}
---
+++ jpa-ex1/src/main/java/hellojpa/Member.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
