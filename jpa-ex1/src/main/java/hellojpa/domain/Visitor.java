1401bf24a45f1fa1bbc8f13a981ab90138aa2ca5
yeon-06 <rnjstldus2@gmail.com>
2022-06-20 18:40:27 +0900

    refactor: 자동 키 생성 옵션 변경
    - AUTO: DB 방언에 따라 아래 3가지 중 자동 선택
    - IDENTITY: DB가 결정
    - SEQUENCE: DB Sequence Object 이용 (@SequenceGenerator 필요)
    - TABLE: 키 생성 전용 테이블을 하나 만들어서 DB 시퀀스를 흉내

--- jpa-ex1/src/main/java/hellojpa/domain/Visitor.java
+++ jpa-ex1/src/main/java/hellojpa/domain/Visitor.java
@@ -4,13 +4,14 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Visitor {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 50, nullable = false)
