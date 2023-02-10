b05580edcb2c3128fb73dbd3b9730da387190160
yeon-06 <yeonlog06@gmail.com>
2023-02-10 21:14:13 +0900

    feat: 단건 조회, 다건 조회, 페이징, 집합 예회

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -11,8 +11,10 @@ import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 
 @SpringBootTest
@@ -22,15 +24,27 @@ class QuerydslApplicationTests {
     @Autowired
     private EntityManager entityManager;
 
+    private JPAQueryFactory queryFactory;
+
     private Member member1;
+    private Member member2;
+    private Member member3;
 
     @BeforeEach
     void init() {
+        queryFactory = new JPAQueryFactory(entityManager);
+
         Team team1 = new Team("team A");
         entityManager.persist(team1);
+        Team team2 = new Team("team B");
+        entityManager.persist(team2);
 
         member1 = new Member("yeonlog", 27, team1);
+        member2 = new Member("logyeon", 25, team2);
+        member3 = new Member("logyeon", 20, team2);
         entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
     }
 
     @Test
@@ -47,12 +61,12 @@ class QuerydslApplicationTests {
         assertThat(findMember.getUsername()).isEqualTo(username);
     }
 
+    // 단건 조회
     @Test
     void selectWithQuerydsl() {
         // given
         String username = member1.getUsername();
-        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
-        QMember qMember = QMember.member; // static으로 선언되어 있어서 바로 사용하면 된다.
+        QMember qMember = member; // static으로 선언되어 있어서 바로 사용하면 된다.
 //        QMember qMember = new QMember("m");   // 같은 테이블을 join하는 경우 선언해서 사용해야 한다.
 
         // when
@@ -63,5 +77,43 @@ class QuerydslApplicationTests {
 
         // then
         assertThat(findMember.getUsername()).isEqualTo(username);
+    }
+
+    // 다건 조회
+    @Test
+    void selectAll() {
+        // given & when=
+        List<Member> members = queryFactory.select(member)
+                .from(member)
+                .fetch();
+
+        // then
+        assertThat(members).hasSize(3);
+    }
+
+    // 페이징 예제
+    @Test
+    void paging() {
+        // given & when
+        List<Member> members = queryFactory.selectFrom(member)
+                .orderBy(member.age.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        // then
+        assertThat(members).hasSize(2);
+    }
+
+    // 집함 함수 예제 - max, min, avg, ...
+    @Test
+    void max() {
+        // given & when
+        Integer result = queryFactory.select(member.age.max())
+                .from(member)
+                .fetchOne();
+
+        // then
+        assertThat(result).isEqualTo(27);
     }
 }
