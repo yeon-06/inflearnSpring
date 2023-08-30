b82dc8db0b5643aa1448ceb728efee64a7217a83
yeon-06 <yeonlog06@gmail.com>
2023-08-30 22:08:09 +0900

    test: 동시성 문제를 확인하기 위한 테스트 코드 작성

--- advanced/src/test/java/hello/advanced/v3/FieldLogTraceTest.java
+++ advanced/src/test/java/hello/advanced/v3/FieldLogTraceTest.java
@@ -1,9 +1,14 @@
 package hello.advanced.v3;
 
 import hello.advanced.trace.TraceStatus;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 
 class FieldLogTraceTest {
+
+    // 콘솔에 출력되는 결과를 확인하기 위해 작성한 코드일뿐, 유의미한 테스트가 아니다.
 
     @Test
     void begin_end() {
@@ -21,5 +26,29 @@ class FieldLogTraceTest {
         TraceStatus status2 = trace.begin("hello2");
         trace.exception(status2, new IllegalStateException());
         trace.exception(status1, new IllegalStateException());
+    }
+
+    // 실행해보면 A와 B가 실행이 구분되지 않고, 로그가 이상하게 출력된다.
+    @Test
+    void concurrency_problem() throws InterruptedException {
+        FieldLogTrace trace = new FieldLogTrace();
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        executor.submit(() -> execute("A", trace, countDownLatch));
+        executor.submit(() -> execute("B", trace, countDownLatch));
+        countDownLatch.await();
+    }
+
+    private void execute(String executeId, FieldLogTrace trace, CountDownLatch countDownLatch) {
+        try {
+            TraceStatus status1 = trace.begin(executeId + "1");
+            TraceStatus status2 = trace.begin(executeId + "2");
+            trace.end(status2);
+            trace.end(status1);
+        } finally {
+            countDownLatch.countDown();
+        }
     }
 }
